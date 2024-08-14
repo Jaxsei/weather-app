68d80ad61dfc633bc5a9b1689166036339ba30f4
jaxsei <thakuriamohan18@gmail.com>
2024-08-14 05:41:29 +0530

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+const apiKey = "bc9c852f44ab4ff98ee235609241308";                                                              
+const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=germany`;
+
+async function checkWeather() {
+  const response = await fetch(apiUrl);
+  var data = await response.json();
+  console.log(data);
+  // document.querySelector(".city").innerHTML = data.location.name;
+  // document.querySelector(".temp").innerHTML = Math.round(data.current.temp_c) + "°c";
+  // document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+  // document.querySelector(".wind").innerHTML = data.current.wind_kph + " km/h";
+}
+
+checkWeather();
